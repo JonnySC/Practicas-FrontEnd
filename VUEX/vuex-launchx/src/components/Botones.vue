552f96c20559f0b87cab31536025d3e01d430ef5
JonnySC <correojonnysc@gmail.com>
2022-03-30 23:45:08 -0600

    actualizacion

---
+++ VUEX/vuex-launchx/src/components/Botones.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="buttons">
+      <button @click="$store.dispatch('bajarContador')" >-</button>
+      <button @click="$store.dispatch('subirContador')" >+</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Botones'
+}
+</script>
---
+++ VUEX/vuex-launchx/src/components/ContadorCuadrado.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="counterSquare">
+      {{ $store.state.counter }}
+      <sup>2</sup> = {{ $store.getters.contadorCuadrado }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ContadorCuadrado'
+}
+</script>
