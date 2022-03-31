ada73702c8459f46ca561cf1baab89a372b9477d
JonnySC <correojonnysc@gmail.com>
2022-03-31 17:07:16 -0600

    actualizacion

--- ANGULAR/launchx/src/app/app.module.ts
+++ ANGULAR/launchx/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchxComponent } from './launchx/launchx.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchxComponent
   ],
   imports: [
     BrowserModule,
